01203cb1694eeb9b810fa84895d90895b381e900
Shanil Jasani <shaniljasani@utexas.edu>
2021-11-06 12:40:24 -0500

    Working on deploying backend

--- webapp/backend/app.py
+++ webapp/backend/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
-from api.ApiHandler import HelloApiHandler
+from ApiHandler import HelloApiHandler
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__, static_url_path='', static_folder='../frontend/build')
 CORS(app) #comment this on deployment
 api = Api(app)
 
