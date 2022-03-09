6a7f3a78e1ada1001d2993e7ded03c355e5844d9
Shanil Jasani <shaniljasani@utexas.edu>
2022-03-09 11:42:02 -0600

    Placeholder for Final Charts
    `/fuel-type-generation` and `/wind-and-solar`

--- webapp/resources/database.py
+++ webapp/resources/database.py
@@ -49,12 +49,46 @@ def get_chart(chart_type, start_date, end_date):
         ch_labels = combine_date_time_24bug(ch_days_dt, ch_times)
         return ch_data, ch_labels
 
+    if chart_type == "fuel-type-generation":
+        # PLACEHOLDER
+        df = pd.read_sql_table("RTSL", connection)
+        if (start_date and end_date):
+            df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
+            df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
+        ch_data = df["Valley"].tolist()
+        ch_days = df['OperatingDay'].tolist()
+        ch_times = df['HourEnding'].tolist()
+
+        ch_days_dt = []
+        for day in ch_days:
+            ch_days_dt.append(pd.Timestamp.to_pydatetime(day))
+        
+        ch_labels = combine_date_time_24bug(ch_days_dt, ch_times)
+        return ch_data, ch_labels
+
     if chart_type == "system-frequency":
         df = pd.read_sql_table("RTSC", connection)
         if (start_date and end_date):
             df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
             df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
         ch_data = df["CurrentFrequency"].tolist()
+        ch_days = df['OperatingDay'].tolist()
+        ch_times = df['HourEnding'].tolist()
+
+        ch_days_dt = []
+        for day in ch_days:
+            ch_days_dt.append(pd.Timestamp.to_pydatetime(day))
+        
+        ch_labels = combine_date_time(ch_days_dt, ch_times)
+        return ch_data, ch_labels
+    
+    if chart_type == "wind-and-solar":
+        # HALFWAY A PLACEHOLDER
+        df = pd.read_sql_table("SPP", connection)
+        if (start_date and end_date):
+            df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
+            df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
+        ch_data = df['SystemWide'].tolist()
         ch_days = df['OperatingDay'].tolist()
         ch_times = df['HourEnding'].tolist()
 
