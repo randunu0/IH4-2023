49896884a3804529ce97a45cb67ade5654af8555
Shanil Jasani <shaniljasani@utexas.edu>
2022-03-09 02:23:38 -0600

    /chart/electricity-prices (INCOMPLETE)

--- webapp/resources/database.py
+++ webapp/resources/database.py
@@ -65,20 +65,66 @@ def get_chart(chart_type, start_date, end_date):
         
         ch_labels = combine_date_time(ch_days_dt, ch_times)
         return ch_data, ch_labels
-    
-    if chart_type == "SPP":
+
+    if chart_type == "electricity-prices":
+        df = pd.read_sql_table("SMPP_LZ", connection)
         if (start_date and end_date):
-            df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
-            df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
-        ch_data = df['SystemWide'].tolist()
-        ch_days = df['OperatingDay'].tolist()
-        ch_times = df['HourEnding'].tolist()
+            df = df[df['DeliveryDate'] >= pd.Timestamp(start_date)]
+            df = df[df['DeliveryDate'] <= pd.Timestamp(end_date)]
+        ch_data = df["SettlementPointPrice"].tolist()
+        ch_days = df['DeliveryDate'].tolist()
+        ch_times = df['DeliveryHour'].tolist()
 
         ch_days_dt = []
         for day in ch_days:
             ch_days_dt.append(pd.Timestamp.to_pydatetime(day))
         
-        ch_labels = combine_date_time(ch_days_dt, ch_times)
+        ch_labels = combine_date_time_24bug(ch_days_dt, ch_times)
         return ch_data, ch_labels
+
+    # if chart_type == "RTSL":
+    #     if (start_date and end_date):
+    #         df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
+    #         df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
+    #     ch_data = df["Valley"].tolist()
+    #     ch_days = df['OperatingDay'].tolist()
+    #     ch_times = df['HourEnding'].tolist()
+
+    #     ch_days_dt = []
+    #     for day in ch_days:
+    #         ch_days_dt.append(pd.Timestamp.to_pydatetime(day))
+        
+    #     ch_labels = combine_date_time_24bug(ch_days_dt, ch_times)
+    #     return ch_data, ch_labels
+    
+    # if chart_type == "SWL":
+    #     if (start_date and end_date):
+    #         df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
+    #         df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
+    #     ch_data = df['Demand'].tolist()
+    #     ch_days = df['OperatingDay'].tolist()
+    #     ch_times = df['HourEnding'].tolist()
+
+    #     ch_days_dt = []
+    #     for day in ch_days:
+    #         ch_days_dt.append(pd.Timestamp.to_pydatetime(day))
+        
+    #     ch_labels = combine_date_time(ch_days_dt, ch_times)
+    #     return ch_data, ch_labels
+    
+    # if chart_type == "SPP":
+    #     if (start_date and end_date):
+    #         df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
+    #         df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
+    #     ch_data = df['SystemWide'].tolist()
+    #     ch_days = df['OperatingDay'].tolist()
+    #     ch_times = df['HourEnding'].tolist()
+
+    #     ch_days_dt = []
+    #     for day in ch_days:
+    #         ch_days_dt.append(pd.Timestamp.to_pydatetime(day))
+        
+    #     ch_labels = combine_date_time(ch_days_dt, ch_times)
+    #     return ch_data, ch_labels
 
     return df
