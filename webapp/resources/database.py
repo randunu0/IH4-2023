b31be54a0539e32ae249594ea6e7b3746f9b8308
Shanil Jasani <shaniljasani@utexas.edu>
2022-04-04 12:13:30 -0500

    Revert "Merge branch 'deployment-notes' into charts-round-2"
    This reverts commit afc0259e000a1684c087662565aa4a9953026bf3, reversing
    changes made to 465239bbf6e9d942fb094a185d86429977ade847.

--- webapp/resources/database.py
+++ webapp/resources/database.py
@@ -31,13 +31,22 @@ def get_chart(chart_type, start_date, end_date):
                                                           database_ip, database_name))
     connection = database_connection.connect() 
 
-
+    # -----------------------------------
+    # System Wide Demand
+    # -----------------------------------
     if chart_type == "system-wide-demand":
-        df = pd.read_sql_table("RTSL", connection)
+        df = pd.read_sql_table("SWD", connection)
         if (start_date and end_date):
             df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
             df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
-        ch_data = df["Valley"].tolist()
+        else:
+            # DEFAULT VIEW: one week
+            end_date = df.iloc[-1].get('OperatingDay')
+            start_date = end_date - pd.Timedelta(days=7)
+            df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
+            df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]                        
+        ch_data_mw = df["Demand"].tolist()
+        ch_data = [entry / 1000 for entry in ch_data_mw]
         ch_days = df['OperatingDay'].tolist()
         ch_times = df['HourEnding'].tolist()
 
@@ -45,15 +54,25 @@ def get_chart(chart_type, start_date, end_date):
         for day in ch_days:
             ch_days_dt.append(pd.Timestamp.to_pydatetime(day))
         
-        ch_labels = combine_date_time_24bug(ch_days_dt, ch_times)
+        ch_labels = combine_date_time(ch_days_dt, ch_times)
         return ch_data, ch_labels
 
+    # -----------------------------------
+    # Fuel Type Generation
+    # TODO complete graph
+    # -----------------------------------
     if chart_type == "fuel-type-generation":
         # PLACEHOLDER
         df = pd.read_sql_table("RTSL", connection)
         if (start_date and end_date):
             df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
             df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
+        else:
+            # DEFAULT VIEW: one week
+            end_date = df.iloc[-1].get('OperatingDay')
+            start_date = end_date - pd.Timedelta(days=7)
+            df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
+            df = df[df['OperatingDay'] <= pd.Timestamp(end_date)] 
         ch_data = df["Valley"].tolist()
         ch_days = df['OperatingDay'].tolist()
         ch_times = df['HourEnding'].tolist()
@@ -65,11 +84,21 @@ def get_chart(chart_type, start_date, end_date):
         ch_labels = combine_date_time_24bug(ch_days_dt, ch_times)
         return ch_data, ch_labels
 
+    # -----------------------------------
+    # System Frequency
+    # TODO hour selector
+    # -----------------------------------
     if chart_type == "system-frequency":
         df = pd.read_sql_table("RTSC", connection)
         if (start_date and end_date):
             df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
             df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
+        else:
+            # DEFAULT VIEW: one week
+            end_date = df.iloc[-1].get('OperatingDay')
+            start_date = end_date - pd.Timedelta(days=1)
+            df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
+            df = df[df['OperatingDay'] <= pd.Timestamp(end_date)] 
         ch_data = df["CurrentFrequency"].tolist()
         ch_days = df['OperatingDay'].tolist()
         ch_times = df['HourEnding'].tolist()
@@ -81,23 +110,60 @@ def get_chart(chart_type, start_date, end_date):
         ch_labels = combine_date_time(ch_days_dt, ch_times)
         return ch_data, ch_labels
     
+    # -----------------------------------
+    # Wind and Solar
+    # TODO add second chart on top
+    # -----------------------------------
     if chart_type == "wind-and-solar":
-        # HALFWAY A PLACEHOLDER
         df = pd.read_sql_table("SPP", connection)
         if (start_date and end_date):
             df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
             df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
-        ch_data = df['SystemWide'].tolist()
-        ch_days = df['OperatingDay'].tolist()
-        ch_times = df['HourEnding'].tolist()
+        else:
+            # DEFAULT VIEW: one week
+            end_date = df.iloc[-1].get('OperatingDay')
+            start_date = end_date - pd.Timedelta(days=7)
+            df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
+            df = df[df['OperatingDay'] <= pd.Timestamp(end_date)] 
+        solar_data = df['SystemWide'].tolist()
 
-        ch_days_dt = []
-        for day in ch_days:
-            ch_days_dt.append(pd.Timestamp.to_pydatetime(day))
+        # Solar Times are the same as Wind Times so disregard one
+        # solar_days = df['OperatingDay'].tolist()
+        # solar_times = df['HourEnding'].tolist()
+
+        # solar_days_dt = []
+        # for day in solar_days:
+        #     solar_days_dt.append(pd.Timestamp.to_pydatetime(day))
         
-        ch_labels = combine_date_time(ch_days_dt, ch_times)
-        return ch_data, ch_labels
+        # solar_labels = combine_date_time(solar_days_dt, solar_times)
 
+        df = pd.read_sql_table("WPP", connection)
+        if (start_date and end_date):
+            df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
+            df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
+        else:
+            # DEFAULT VIEW: one week
+            end_date = df.iloc[-1].get('OperatingDay')
+            start_date = end_date - pd.Timedelta(days=7)
+            df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
+            df = df[df['OperatingDay'] <= pd.Timestamp(end_date)] 
+        wind_data = df['SystemWide'].tolist()
+        wind_days = df['OperatingDay'].tolist()
+        wind_times = df['HourEnding'].tolist()
+
+        wind_days_dt = []
+        for day in wind_days:
+            wind_days_dt.append(pd.Timestamp.to_pydatetime(day))
+        
+        wind_labels = combine_date_time(wind_days_dt, wind_times)
+
+
+        return [wind_data, solar_data], wind_labels
+
+    # -----------------------------------
+    # Electricity Prices
+    # TODO connect to db
+    # -----------------------------------
     if chart_type == "electricity-prices":
         df = pd.read_sql_table("SMPP_LZ", connection)
         if (start_date and end_date):
@@ -114,81 +180,5 @@ def get_chart(chart_type, start_date, end_date):
         ch_labels = combine_date_time_24bug(ch_days_dt, ch_times)
         return ch_data, ch_labels
 
-    df = pd.read_sql_table(chart_type, connection)
+    return pd.read_sql_table(chart_type, connection)
 
-    if chart_type == "RTSL":
-        if (start_date and end_date):
-            df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
-            df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
-        ch_data = df["Valley"].tolist()
-        ch_days = df['OperatingDay'].tolist()
-        ch_times = df['HourEnding'].tolist()
-
-        ch_days_dt = []
-        for day in ch_days:
-            ch_days_dt.append(pd.Timestamp.to_pydatetime(day))
-        
-        ch_labels = combine_date_time_24bug(ch_days_dt, ch_times)
-        return ch_data, ch_labels
-    
-    if chart_type == "RTSC":
-        if (start_date and end_date):
-            df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
-            df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
-        ch_data = df["CurrentFrequency"].tolist()
-        ch_days = df['OperatingDay'].tolist()
-        ch_times = df['HourEnding'].tolist()
-
-        ch_days_dt = []
-        for day in ch_days:
-            ch_days_dt.append(pd.Timestamp.to_pydatetime(day))
-        
-        ch_labels = combine_date_time_24bug(ch_days_dt, ch_times)
-        return ch_data, ch_labels
-
-    if chart_type == "SWL":
-        if (start_date and end_date):
-            df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
-            df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
-        ch_data = df['Demand'].tolist()
-        ch_days = df['OperatingDay'].tolist()
-        ch_times = df['HourEnding'].tolist()
-
-        ch_days_dt = []
-        for day in ch_days:
-            ch_days_dt.append(pd.Timestamp.to_pydatetime(day))
-        
-        ch_labels = combine_date_time(ch_days_dt, ch_times)
-        return ch_data, ch_labels
-    
-    if chart_type == "SPP":
-        if (start_date and end_date):
-            df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
-            df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
-        ch_data = df['SystemWide'].tolist()
-        ch_days = df['OperatingDay'].tolist()
-        ch_times = df['HourEnding'].tolist()
-
-        ch_days_dt = []
-        for day in ch_days:
-            ch_days_dt.append(pd.Timestamp.to_pydatetime(day))
-        
-        ch_labels = combine_date_time(ch_days_dt, ch_times)
-        return ch_data, ch_labels
-
-    if chart_type == "WPP":
-        if (start_date and end_date):
-            df = df[df['OperatingDay'] >= pd.Timestamp(start_date)]
-            df = df[df['OperatingDay'] <= pd.Timestamp(end_date)]
-        ch_data = df['SystemWide'].tolist()
-        ch_days = df['OperatingDay'].tolist()
-        ch_times = df['HourEnding'].tolist()
-
-        ch_days_dt = []
-        for day in ch_days:
-            ch_days_dt.append(pd.Timestamp.to_pydatetime(day))
-        
-        ch_labels = combine_date_time(ch_days_dt, ch_times)
-        return ch_data, ch_labels    
-
-    return df
